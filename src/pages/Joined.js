caa5fd303271164d2df7dad405f08bcfb15dc735
Alfredo Patricius Tarigan <62396803+alfredoptarigan@users.noreply.github.com>
2020-10-23 12:34:12 +0700

    Preparing Error Page and Joined Page

--- src/pages/Joined.js
+++ src/pages/Joined.js
@@ -2,23 +2,25 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-export default function NotFound() {
+export default function Joined({ history, match }) {
+  function joining() {}
   return (
     <section className="h-screen flex flex-col items-center">
       <img
-        src={`${process.env.PUBLIC_URL}/assets/images/illustration-notfound.jpg`}
-        alt="Ooops we lost you"
+        src={`${process.env.PUBLIC_URL}/assets/images/illustration-joined.jpg`}
+        alt="Join Class"
       />
-      <h1 className="text-3xl text-gray-900 mt-12">Opps! We’re lost</h1>
+      <h1 className="text-3xl text-gray-900 mt-12">Welcome to Class</h1>
       <p className="text-lg text-gray-600 mt-4 mtb-8 lg:w-3/12 xl:w-2/12 mx-auto text-center">
-        The page that you requested is not found in our system
+        You have successfully joined our <strong></strong> class
       </p>
-      <Link
+      <span
+        onClick={joining}
         className="bg-orange-500 hover:bg-orange-400 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 mt-5"
         to="/"
       >
-        Back to home
-      </Link>
+        Start Learn
+      </span>
     </section>
   );
 }
